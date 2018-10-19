8fd4614602e1a6e55c8fa79ef7d8e89bcc1e8157
Certil Remy <certil@noukod.com>
2018-10-19 15:33:13 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -12,4 +12,19 @@ class BankAccount
   def deposit(value)
     @balance+=value
   end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status=="open" && @balance > 0
+  end
+
+  def close_account
+    @status="closed"
+  end
+
+
+
 end
